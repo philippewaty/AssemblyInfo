1beee794abaaed25437f4f6b2cabea0a204ecbae
Philippe Waty <47305204+philippewaty@users.noreply.github.com>
2022-11-07 09:51:07 +0100

    Refactor to static methods

--- AssemblyInfo/AssemblyInfo.cs
+++ AssemblyInfo/AssemblyInfo.cs
@@ -8,112 +8,107 @@ using System.Runtime.Versioning;
 
 namespace AssemblyInfo
 {
-  public class AssemblyInfo
+  public static class AssemblyInfo
   {
-    private Assembly _objParentAssembly = null;
-
-    private Assembly ParentAssembly()
+    /// <summary>
+    /// Returns the parent assembly
+    /// </summary>
+    /// <returns></returns>
+    private static Assembly ParentAssembly()
     {
-      if (_objParentAssembly == null)
+      Assembly _objParentAssembly;
+      if (Assembly.GetEntryAssembly() == null)
       {
-        if (Assembly.GetEntryAssembly() == null)
-        {
-          _objParentAssembly = Assembly.GetCallingAssembly();
-        }
-        else
-        {
-          _objParentAssembly = Assembly.GetEntryAssembly();
-        }
+        _objParentAssembly = Assembly.GetCallingAssembly();
+      }
+      else
+      {
+        _objParentAssembly = Assembly.GetEntryAssembly();
       }
       return _objParentAssembly;
     }
 
-    public AssemblyInfo()
-    {
-      _objParentAssembly = ParentAssembly();
-    }
-
-    public String Title
+    public static String Title
     {
       get
       {
-        var applicationTitle = ((AssemblyTitleAttribute)_objParentAssembly.GetCustomAttributes(typeof(AssemblyTitleAttribute), false)[0]).Title;
+        var applicationTitle = ((AssemblyTitleAttribute)ParentAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false)[0]).Title;
         if (string.IsNullOrWhiteSpace(applicationTitle))
         {
-          applicationTitle = _objParentAssembly.GetName().Name;
+          applicationTitle = ParentAssembly().GetName().Name;
         }
         return applicationTitle;
       }
     }
 
-    public string Description
+    public static string Description
     {
       get
       {
-        var applicationDescription = ((AssemblyDescriptionAttribute)_objParentAssembly.GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false)[0]).Description;
+        var applicationDescription = ((AssemblyDescriptionAttribute)ParentAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false)[0]).Description;
         return applicationDescription;
       }
     }
 
-    public string Company
+    public static string Company
     {
       get
       {
-        var applicationCompany = ((AssemblyCompanyAttribute)_objParentAssembly.GetCustomAttributes(typeof(AssemblyCompanyAttribute), false)[0]).Company;
+        var applicationCompany = ((AssemblyCompanyAttribute)ParentAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false)[0]).Company;
         return applicationCompany;
       }
     }
 
-    public string Product
+    public static string Product
     {
       get
       {
-        var applicationProduct = ((AssemblyProductAttribute)_objParentAssembly.GetCustomAttributes(typeof(AssemblyProductAttribute), false)[0]).Product;
+        var applicationProduct = ((AssemblyProductAttribute)ParentAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false)[0]).Product;
         return applicationProduct;
       }
     }
 
-    public string Copyright
+    public static string Copyright
     {
       get
       {
-        var applicationCopyright = ((AssemblyCopyrightAttribute)_objParentAssembly.GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false)[0]).Copyright;
+        var applicationCopyright = ((AssemblyCopyrightAttribute)ParentAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false)[0]).Copyright;
         return applicationCopyright;
       }
     }
 
-    public string Version
+    public static string Version
     {
       get
       {
-        var applicationVersion = ((AssemblyFileVersionAttribute)_objParentAssembly.GetCustomAttributes(typeof(AssemblyFileVersionAttribute), false)[0]).Version;
+        var applicationVersion = ((AssemblyFileVersionAttribute)ParentAssembly().GetCustomAttributes(typeof(AssemblyFileVersionAttribute), false)[0]).Version;
         return applicationVersion;
       }
     }
 
     // https://visualstudiomagazine.com/articles/2016/12/01/assembly-by-name-and-number.aspx#:~:text=The%20location%20of%20the%20assembly%27s%20version%20number%20appears,for%20the%20Major%2C%20Minor%2C%20Build%20and%20Revision%20values%3A
-    public System.Version FullVersion
+    public static System.Version FullVersion
     {
       get
       {
-        var applicationVersion = _objParentAssembly.GetName().Version;
+        var applicationVersion = ParentAssembly().GetName().Version;
         return applicationVersion;
       }
     }
 
-    public string Trademark
+    public static string Trademark
     {
       get
       {
-        var value = ((AssemblyTrademarkAttribute)_objParentAssembly.GetCustomAttributes(typeof(AssemblyTrademarkAttribute), false)[0]).Trademark;
+        var value = ((AssemblyTrademarkAttribute)ParentAssembly().GetCustomAttributes(typeof(AssemblyTrademarkAttribute), false)[0]).Trademark;
         return value;
       }
     }
-    public string Framework
+    public static string Framework
     {
       get
       {
-        var applicationFramework = ((TargetFrameworkAttribute)_objParentAssembly.GetCustomAttributes(typeof(TargetFrameworkAttribute), false)[0]).FrameworkDisplayName;
+        var applicationFramework = ((TargetFrameworkAttribute)ParentAssembly().GetCustomAttributes(typeof(TargetFrameworkAttribute), false)[0]).FrameworkDisplayName;
         return applicationFramework;
       }
     }
--- README.md
+++ README.md
@@ -3,16 +3,15 @@ Library returning assembly info of the parent assembly
 
 Code sample :
 ```C#
-AssemblyInfo.AssemblyInfo assemblyInfo = new AssemblyInfo.AssemblyInfo();
-Console.WriteLine($"Title={assemblyInfo.Title}");
-Console.WriteLine($"Description={assemblyInfo.Description}");
-Console.WriteLine($"Company={assemblyInfo.Company}");
-Console.WriteLine($"Product={assemblyInfo.Product}");
-Console.WriteLine($"Copyright={assemblyInfo.Copyright}");
-Console.WriteLine($"Version={assemblyInfo.Version}");
-Console.WriteLine($"Full version = Major:{assemblyInfo.FullVersion.Major}, Minor:{assemblyInfo.FullVersion.Minor}, Revision:{assemblyInfo.FullVersion.Revision}");
-Console.WriteLine($"Trademark={assemblyInfo.Trademark}");
-Console.WriteLine($"Framework={assemblyInfo.Framework}");
+Console.WriteLine($"Title={AssemblyInfo.AssemblyInfo.Title}");
+Console.WriteLine($"Description={AssemblyInfo.AssemblyInfo.Description}");
+Console.WriteLine($"Company={AssemblyInfo.AssemblyInfo.Company}");
+Console.WriteLine($"Product={AssemblyInfo.AssemblyInfo.Product}");
+Console.WriteLine($"Copyright={AssemblyInfo.AssemblyInfo.Copyright}");
+Console.WriteLine($"Version={AssemblyInfo.AssemblyInfo.Version}");
+Console.WriteLine($"Full version = Major:{AssemblyInfo.AssemblyInfo.FullVersion.Major}, Minor:{AssemblyInfo.AssemblyInfo.FullVersion.Minor}, Revision:{AssemblyInfo.AssemblyInfo.FullVersion.Revision}");
+Console.WriteLine($"Trademark={AssemblyInfo.AssemblyInfo.Trademark}");
+Console.WriteLine($"Framework={AssemblyInfo.AssemblyInfo.Framework}");
 ```
 
 	  
