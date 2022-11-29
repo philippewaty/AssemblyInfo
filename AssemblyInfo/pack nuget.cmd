58e2567ab5059900d7d6d84528aa162e2036c96f
Philippe Waty <philippe.waty@medsoc.be>
2022-11-29 09:42:10 +0100

    Remove nuget binary

--- AssemblyInfo/pack nuget.cmd
+++ AssemblyInfo/pack nuget.cmd
@@ -2,6 +2,6 @@ if not exist NuGet mkdir NuGet
 
 del /Q NuGet\*.*
 
-.nuget\NuGet.exe pack AssemblyInfo.nuspec -OutputDirectory NuGet -IncludeReferencedProjects -Properties Configuration=Release;Platform=AnyCPU -Build
+NuGet.exe pack AssemblyInfo.nuspec -OutputDirectory NuGet -IncludeReferencedProjects -Properties Configuration=Release;Platform=AnyCPU -Build
 
 rem pause
